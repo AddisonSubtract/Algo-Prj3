208b9c7255b89750f90adb31c1d7413d951e7358
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2022-04-06 10:39:09 -0500

    Create networkXGraph1.py

---
+++ networkXGraph1.py
@@ -0,0 +1,28 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+G = nx.Graph()
+
+G.add_nodes_from("ABCDEFGHIJKLMNOP")
+
+
+#Edge creation
+edges =[('A','B'),('A','E'),('A','F'),('B','C'),('B','F'),
+         ('C','D'),('C','G'),('D','G'),('E','F'),('E','I'),
+         ('F','I'),('G','J'),('H','K'),('H','L'),('I','J'),
+         ('I','M'),('K','L'),('K','O'),('L','P'),('M','N')]
+G.add_edges_from(edges)
+
+#print(G.to_undirected(G))
+
+nx.draw(G, with_labels=1)
+plt.show()
+
+#Question a
+print("Node A DFS:", list(nx.dfs_edges(G, source='A')),'\n')
+print("Node A BFS:", list(nx.bfs_edges(G, source='A')),'\n')
+print("Node H DFS:", list(nx.dfs_edges(G, source='H')),'\n')
+print("Node H BFS:", list(nx.bfs_edges(G, source='H')),'\n')
+
+#Question c
